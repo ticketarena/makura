74f69cb956844d5eea272637e4b552fd2044381d
Ell Neal <elliot@emdentec.com>
2015-05-27 13:26:32 +0100

    Add default timeout of infinite proportions

--- lib/makura/http_methods.rb
+++ lib/makura/http_methods.rb
@@ -1,19 +1,19 @@
 module Makura
   module HTTPMethods
     def delete(path, params = {})
-      request(:delete, path, params)
+      request(:delete, path, { timeout: -1, open_timeout: -1 }.merge(params || {}))
     end
 
     def get(path, params = {})
-      request(:get, path, params)
+      request(:get, path, { timeout: -1, open_timeout: -1 }.merge(params || {}))
     end
 
     def post(path, params = {})
-      request(:post, path, params)
+      request(:post, path, { timeout: -1, open_timeout: -1 }.merge(params || {}))
     end
 
     def put(path, params = {})
-      request(:put, path, params)
+      request(:put, path, { timeout: -1, open_timeout: -1 }.merge(params || {}))
     end
   end
 end
